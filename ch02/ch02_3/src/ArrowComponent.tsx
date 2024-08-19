572ff623fe0b1d2d6684d19a9bd7f72048b19489
JOO956924 <wndbs000@gmail.com>
2024-08-19 16:36:27 +0900

    2024 08 19

--- ch02/ch02_3/src/ArrowComponent.tsx
+++ ch02/ch02_3/src/ArrowComponent.tsx
@@ -3,6 +3,7 @@ export type ArrowComponentProps = {
   href: string, text: string
 }
 
+// 상용구 코드(boilerplate)가 없어서 좀더 간결하게 구현 가능
 const ArrowComponent: FC<ArrowComponentProps> = props => {
   const(href, text) = props
   return(
