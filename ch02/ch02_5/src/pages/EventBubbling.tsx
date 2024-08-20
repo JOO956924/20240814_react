1f7496fbacf589f58165a8949c7d75313162a619
JOO956924 <wndbs000@gmail.com>
2024-08-20 15:01:53 +0900

    2024 08 20

--- ch02/ch02_5/src/pages/EventBubbling.tsx
+++ ch02/ch02_5/src/pages/EventBubbling.tsx
@@ -1,3 +1,20 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+import type {SyntheticEvent} from 'react'
+
+export default function EventBubbling() {
+  const onDivClick = function (e: SyntheticEvent) {
+    const {isTrusted, target, bubbles, currentTarget} = e
+    // prettier-ignore
+    console.log('onDivClick',isTrusted,target,bubbles,currentTarget)
+  }
+  const onButtonClick = (e: SyntheticEvent) => {
+    const {isTrusted, target, bubbles} = e
+    // prettier-ignore
+    console.log('onButtonClick::',isTrusted, target, bubbles)
+  }
+  return (
+    <div onClick={onDivClick}>
+      <span>EventBubbling</span>
+      <button onClick={onButtonClick}>Click me</button>
+    </div>
+  )
 }
--- ch02/ch02_5/src/pages/EventListener.tsx
+++ ch02/ch02_5/src/pages/EventListener.tsx
@@ -1,5 +1,8 @@
 // onclick적용이 아닌 addEventListener('click')일 경우
 // 아래처럼 2번 적용하더라도 모두 적용된다.
+
+// bubbles는 이벤트 대상이 겹칠 경우 겹치는 모든 대상에 이벤트 발생
+// isTrusted는 웹브라우저에서 발생하면 true
 document.querySelector('#root')?.addEventListener('click', function (e: Event) {
   const {isTrusted, target, bubbles} = e
   console.log('EventListener::mouse click occurs!', isTrusted, target, bubbles)
