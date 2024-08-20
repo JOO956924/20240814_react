4f040bb76f74e726285d1c9ac03afa344f07f806
JOO956924 <wndbs000@gmail.com>
2024-08-20 15:41:16 +0900

    2024 08 20

--- ch02/ch02_5/src/pages/StopPropagation.tsx
+++ ch02/ch02_5/src/pages/StopPropagation.tsx
@@ -1,3 +1,21 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+import type {SyntheticEvent} from 'react'
+
+export default function StopPropagation() {
+  const onDivClick = function (e: SyntheticEvent) {
+    const {isTrusted, target, bubbles, currentTarget} = e
+    // prettier-ignore
+    console.log('onDivClick',isTrusted,target,bubbles,currentTarget)
+  }
+  const onButtonClick = (e: SyntheticEvent) => {
+    const {isTrusted, target, bubbles} = e
+    // prettier-ignore
+    console.log('onButtonClick::',isTrusted, target, bubbles)
+    e.stopPropagation()
+  }
+  return (
+    <div onClick={onDivClick}>
+      <span>StopPropagation</span>
+      <button onClick={onButtonClick}>Click me</button>
+    </div>
+  )
 }
